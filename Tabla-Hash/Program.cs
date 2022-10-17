c2c73b9e3f5d97040df3c45a2903f032a4b337fc
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2022-10-16 21:09:05 -0300

    Agregar archivos de proyecto.

---
+++ Tabla-Hash/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_Hash
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> Diccionario = new Dictionary<string, string>();
+            string frase;
+
+            Diccionario.Add("hello", "hola");
+            Diccionario.Add("good bye", "adios");
+            Diccionario.Add("I am", "yo soy");
+            Diccionario.Add("beutiful", "hermoso");
+            Diccionario.Add("farher", "padre");
+            Diccionario.Add("brother", "hermano");
+
+            Console.WriteLine("Ingresa una frase en ingles");
+            frase = Console.ReadLine();
+
+            if (Diccionario.ContainsKey(frase))
+            {
+                Console.WriteLine($"La frase {frase}, se traduce como: {Diccionario[frase]}");
+            }
+            else
+            {
+                Console.WriteLine($"No se ha encontrado la frase '{frase}' en el Diccionario");
+            }
+
+            //conbsola mantenga abierta
+            Console.ReadKey();
+        }
+    }
+}
